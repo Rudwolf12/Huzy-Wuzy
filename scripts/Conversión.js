447e07b9f824772ac2debe69a3b42cc7d66d756c
Rudwolf12 <rodolfopemc@outlook.com>
2024-11-16 03:28:37 -0600

    Mejoras

--- scripts/Conversión.js
+++ scripts/Conversión.js
@@ -2,5 +2,4 @@ faren = prompt("Escribe los grados Fahrenheit para ser convertidos a Celsius")
 
 var Celsi = (5/9) * (faren - 32)
 
-document.write("<p>Grados en Fahrenheit: " + faren + "</p>")
-document.write("<p>Grados en Celsius: " + parseInt(Celsi) + "</p>")
+document.getElementById("resultado").innerHTML = "<p>Grados en Fahrenheit: " + faren + "</p> <p>Grados en Celsius: " + parseInt(Celsi) + "</p>"
